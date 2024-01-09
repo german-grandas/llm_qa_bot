14b25eae3c768232654ac32c419836c41487918c
GermanGrandas <gertiven@gmail.com>
2024-01-09 10:44:58 -0500

    updating Azure Search object to support metadata and multiple k as results of the queries

--- server/vector_storage/azure_vector_storage.py
+++ server/vector_storage/azure_vector_storage.py
@@ -6,10 +6,63 @@ from azure.search.documents.indexes.models import (
     SemanticConfiguration,
     PrioritizedFields,
     SemanticField,
+    SimpleField,
+    SearchFieldDataType,
+    SearchableField,
+    SearchField,
 )
+
 
 VECTOR_STORE_ADDRESS = os.environ.get("VECTOR_STORE_ADDRESS")
 VECTOR_STORE_PASSWORD = os.environ.get("VECTOR_STORE_PASSWORD")
+
+
+def get_fields(embedding_function):
+    fields = [
+        SimpleField(
+            name="id",
+            type=SearchFieldDataType.String,
+            key=True,
+            filterable=True,
+        ),
+        SearchableField(
+            name="content",
+            type=SearchFieldDataType.String,
+            searchable=True,
+        ),
+        SearchField(
+            name="content_vector",
+            type=SearchFieldDataType.Collection(SearchFieldDataType.Single),
+            searchable=True,
+            vector_search_dimensions=len(embedding_function("Text")),
+            vector_search_configuration="default",
+        ),
+        SearchableField(
+            name="metadata",
+            type=SearchFieldDataType.String,
+            searchable=True,
+        ),
+        # Additional field to store the title
+        SearchableField(
+            name="title",
+            type=SearchFieldDataType.String,
+            searchable=True,
+        ),
+        # Additional field for filtering on document source
+        SimpleField(
+            name="source",
+            type=SearchFieldDataType.String,
+            filterable=True,
+        ),
+        # Additional data field for last doc update
+        SimpleField(
+            name="last_update",
+            type=SearchFieldDataType.DateTimeOffset,
+            searchable=True,
+            filterable=True,
+        ),
+    ]
+    return fields
 
 
 class AzureVectorStorage:
@@ -23,6 +76,7 @@ class AzureVectorStorage:
                 azure_search_key=VECTOR_STORE_PASSWORD,
                 index_name=index_name,
                 embedding_function=embeddings.embed_query,
+                fields=get_fields(embeddings.embed_query),
                 semantic_configuration_name="config",
                 semantic_settings=SemanticSettings(
                     default_configuration="config",
@@ -51,7 +105,7 @@ class AzureVectorStorage:
     def search(self, query):
         docs = self.vector_storage.similarity_search(
             query=query,
-            k=1,
+            k=3,
             search_type="hybrid",
         )
         return docs
